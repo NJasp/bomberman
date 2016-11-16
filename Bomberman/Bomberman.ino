35c2947fc9ce9b326259b83f01681aa4be27bb58
Martijn <Martijn@MARTIJN>
2016-11-16 12:50:24 +0100

    hi

--- Bomberman/Bomberman.ino
+++ Bomberman/Bomberman.ino
@@ -6,7 +6,7 @@
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-	Hallo nils dit is een wijziging
+	Hallo nils dit is een wijziging.
 }
 
 // the loop function runs over and over again until power down or reset
