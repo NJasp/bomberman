51d0d15222600ba38ee77eb479c35b2e0494948c
gebruiker-pc <gebruiker-pc@GEBRUIKER>
2016-11-24 15:37:40 +0100

    Half working bomb placement

--- Bomberman/Bomberman.ino
+++ Bomberman/Bomberman.ino
@@ -14,8 +14,10 @@ uint8_t collumnCounter;		//collumnCounter
 uint8_t rowCounter;			//rowCounter
 uint8_t player1_x = 1, player1_y = 1;		//player locations
 uint8_t cirkelgrootte = (gridgrootte / 2) - 1;	//Size of playercircle
-uint8_t player1_x_Speed = 10, player1_y_Speed = 10, player1_xCounter = 0, player1_yCounter = 0;		//Player movement speed
+uint8_t player1_xCounter = 0, player1_yCounter = 0;		//Player movement speed
 uint8_t player1_x_old = 1, player1_y_old = 1;		//Old locations of the player;
+uint8_t player1_x_bombdrop = 0, player1_y_bombdrop = 0;
+uint16_t xx = 0;
 
 void init_Hardware();
 void init_OutsideWalls();
@@ -23,13 +25,13 @@ void draw_OutsideWalls();
 void draw_Grid();
 void view_Griddata();
 void read_Nunchuck();
-void check_Wall();
 void calculate_Movement();
 void draw_Player();
 void check_Bomb();
 void draw_Bomb();
 
 int main() {
+	Serial.begin(9600);
 	init_Hardware();
 	draw_Grid();
 	init_OutsideWalls();
@@ -37,8 +39,8 @@ int main() {
 	for (;;) {	// MAIN LOOP									
 		read_Nunchuck();
 		calculate_Movement();
-		draw_Player();
 		check_Bomb();
+		draw_Player();		
 		draw_Bomb();
 	}
 	return 0;
@@ -64,7 +66,6 @@ void init_OutsideWalls()
 		for (collumnCounter = 0; collumnCounter < 16; collumnCounter++) {
 			if (rowCounter == 0 || rowCounter == 11) {
 				grid[collumnCounter][rowCounter] = 1;
-				grid[4][3] = 1;
 			}
 			else {
 				grid[0][rowCounter] = 1;
@@ -130,15 +131,10 @@ void read_Nunchuck()
 	joy_y_axis = nunchuck_buf[1];
 }
 
-void check_Wall()
-{
-
-}
-
 void calculate_Movement()
 {
 	if (joy_x_axis > 140) {
-		if (player1_xCounter == player1_x_Speed) {
+		if (player1_xCounter == 10) {
 			if (!(grid[player1_x + 1][player1_y])) {
 				player1_x++;	//hier word de teller voor de X coordinaat verhoogt als de joystick naar rechts word gedrukt
 				player1_xCounter = 0;
@@ -152,7 +148,7 @@ void calculate_Movement()
 		}
 	}
 	if (joy_x_axis < 114) {
-		if (player1_xCounter == player1_x_Speed) {
+		if (player1_xCounter == 10) {
 			if (!(grid[player1_x - 1][player1_y])) {
 				player1_x--; //hier word de teller voor de X coordinaat verlaagt als de joystick naar links word gedrukt. komt niet lager als 0
 				player1_xCounter = 0;
@@ -166,7 +162,7 @@ void calculate_Movement()
 		}
 	}
 	if (joy_y_axis > 140) {
-		if (player1_yCounter == player1_y_Speed) {
+		if (player1_yCounter == 10) {
 			if (!(grid[player1_x][player1_y - 1])) {
 				player1_y--;	//hier word de teller voor de Y coordinaat verlaagt als de joystick naar beneden word gedrukt
 				player1_yCounter = 0;
@@ -180,7 +176,7 @@ void calculate_Movement()
 		}
 	}
 	if (joy_y_axis < 114) {
-		if (player1_yCounter == player1_y_Speed) {
+		if (player1_yCounter == 10) {
 			if (!(grid[player1_x][player1_y + 1])) {
 				player1_y++;	//hier word de teller voor de Y coordinaat verhoogt als de joystick naar boven word gedrukt
 				player1_yCounter = 0;
@@ -197,27 +193,31 @@ void calculate_Movement()
 
 void draw_Player()
 {
-	if(player1_y_old != player1_y || player1_x_old != player1_x)
-			lcd.fillCircle((player1_x_old*gridgrootte) + (gridgrootte / 2), (player1_y_old*gridgrootte) + (gridgrootte / 2), cirkelgrootte, RGB(255, 255, 255));
-			lcd.fillCircle((player1_x*gridgrootte) + (gridgrootte / 2), (player1_y*gridgrootte) + (gridgrootte / 2), cirkelgrootte, RGB(255, 0, 0));
-			player1_x_old = player1_x;
-			player1_y_old = player1_y;
+	if (player1_y_old != player1_y || player1_x_old != player1_x) {
+		lcd.fillCircle((player1_x_old*gridgrootte) + (gridgrootte / 2), (player1_y_old*gridgrootte) + (gridgrootte / 2), cirkelgrootte, RGB(255, 255, 255));
+	}
+		lcd.fillCircle((player1_x*gridgrootte) + (gridgrootte / 2), (player1_y*gridgrootte) + (gridgrootte / 2), cirkelgrootte, RGB(255, 0, 0));
+		player1_x_old = player1_x;
+		player1_y_old = player1_y;
+	
 }
 
 void check_Bomb()
 {
 	if (!((nunchuck_buf[5] >> 0) & 1)){
 		grid[player1_x][player1_y] = 2;
+		player1_x_bombdrop = player1_x;
+		player1_y_bombdrop = player1_y;
 	}
 }
 
 void draw_Bomb()
 {
-	for (rowCounter = 0; rowCounter < 12; rowCounter++) {
-		for (collumnCounter = 0; collumnCounter < 16; collumnCounter++) {
-			if (grid[collumnCounter][rowCounter] == 2) {
-				lcd.fillCircle((collumnCounter*gridgrootte) + (gridgrootte / 2), (rowCounter*gridgrootte) + (gridgrootte / 2), cirkelgrootte, RGB(0, 0, 255));
-			}
-		}
+	if (((player1_x_bombdrop) != 0 && (player1_y_bombdrop != 0)) && ((player1_x != player1_x_bombdrop) || (player1_y != player1_y_bombdrop))) {
+		lcd.fillCircle(xx, 20, 20, RGB(0, 255, 0));
+		lcd.fillCircle((player1_x_bombdrop*gridgrootte) + (gridgrootte / 2), (player1_y_bombdrop*gridgrootte) + (gridgrootte / 2), cirkelgrootte, RGB(0, 0, 255));
+		player1_x_bombdrop = 0;
+		player1_y_bombdrop = 0;
+		xx++;
 	}
 }
