ac75a9ba62bb7755a7d3ec55d91d955f4542cd02
Koen Sleurink <koensleurink@hotmail.com>
2016-12-05 14:22:55 +0100

    rollback to the good ol' times

--- Bomberman/Bomberman.ino
+++ Bomberman/Bomberman.ino
@@ -6,7 +6,6 @@
 #include "Libraries/Bomb/Bomb.h"
 #include "Libraries/MSD_shield/mSD_shield.h"
 #include "Libraries/IR/IR.h"
-#include "Libraries/Hit/checkHit.h"
 
 MI0283QT9 lcd;					//LCD variabele
 char *wall_Type = "wall3.bmp";
@@ -27,14 +26,14 @@ uint8_t antiholdCounter = 0;				// 1 when the player holds the 'Z' button, so th
 uint16_t interruptCounter = 0;				//used to count seconds in the interrupt
 uint8_t livebombs = 0;
 uint16_t IRdata, Background = RGB(222, 219, 214);
-uint32_t nTimer = 0, timer = 0;
+uint32_t nTimer = 0;
 
 uint8_t bombradius = 5;
 uint8_t player1_x_speed = 0, player1_y_speed = 0; //Higher is slower
 data_store player2_data;
 uint8_t max_bombs = 5;
 uint8_t score = 0;
-uint8_t hit = 0;
+uint8_t killedPlayer = 0;
 uint8_t lives = 1;
 
 void init_Timer();
@@ -55,11 +54,22 @@ int main() {
 	for (;;) {	// MAIN LOOP	
 		read_Nunchuck(nunchuck_buf, &joy_x_axis, &joy_y_axis);
 		calculate_Movement(&player1_x, &player1_y, joy_x_axis, joy_y_axis, &player1_xCounter, &player1_yCounter, player1_x_speed, player1_y_speed, grid);
+		if (dataReady_IR() == 1) {
+			player2_data = decode_IR(IRdata);
+		}
 		check_Bomb(player1_x, player1_y, &player1_x_bombdrop, &player1_y_bombdrop, max_bombs, &livebombs, &antiholdCounter, nunchuck_buf, grid);
 		draw_Player(player1_x, player1_y, &player1_x_old, &player1_y_old, lcd);
+		lcd.fillCircle(player2_data.xData, player2_data.yData, 10, RGB(0, 0, 255));
 		draw_Bomb(player1_x, player1_y, &player1_x_bombdrop, &player1_y_bombdrop, lcd);
-		draw_Explosion(lcd, bombradius, grid, &livebombs, &score);
+		draw_Explosion(lcd, bombradius, grid, &livebombs, &score, &killedPlayer, player1_x, player1_y, &lives);
 		clear_Explosion(lcd, bombradius, grid);
+		if (killedPlayer) {
+			lcd.fillScreen(RGB(0, 0, 0));
+			lcd.drawText(50, 60, "Game over", RGB(255, 255, 255), RGB(0, 0, 0), 3);
+			lcd.drawText(70, 100, "Score player 1: ", RGB(255, 255, 255), RGB(0, 0, 0), 1);
+			lcd.drawInteger(200, 100, score, 10, RGB(255, 255, 255), RGB(0, 0, 0), 1);
+			while (1);
+		}
 	}
 	return 0;
 }
@@ -68,14 +78,14 @@ void init_Timer() {
 	TCCR2A = 0;
 	TCCR2B = 0;
 	TIMSK2 = 0;
-	TCCR2A = (1 << COM2B0) | (1 << WGM21); 	// toggle OC2B on match
-	TCCR2B |= (1 << CS21); 					// 8 prescaler
-	TIMSK2 |= (1 << OCIE2A);				// enable compare interrupt|
-	OCR2B = 26; 					// value to compare timer against	| 1/((2*26)*(1/16000000)*8) = 37,7kHz
-	OCR2A = 26;								// counter
+	TCCR2A = (1 << COM2B0); 	// toggle OC2A on match
+	TCCR2B |= (1 << CS21); 		// 8 prescaler		
+	TIMSK2 |= (1 << TOIE2) | (1 << OCIE2A);		// enable overflow interrupt|
+	OCR2B = 53; 				// value to compare timer against	| 1/(53*(1/16000000)*8) = 37,7kHz
+	OCR2A = 2;								// nanosecond counter
+	TCNT2 = 0;										//SET TIMER 2 AAN (Prescaling 1/1024)
 	sei();
 }
-
 
 ISR(TIMER2_OVF_vect) {		//3906 voor een halve seconde (ongeveer)
 	if (interruptCounter >= 3906 /*3906*/) {
@@ -93,13 +103,22 @@ ISR(TIMER2_OVF_vect) {		//3906 voor een halve seconde (ongeveer)
 	}
 }
 
-/*ISR(TIMER2_COMPA_vect) {// 10 nano second timer
-nTimer++;
-// send function
-if (IR_isSending()) {
-IR_processSend(nTimer);
+ISR(TIMER2_COMPA_vect) { // timer for receiving/sending
+	nTimer++;
+
+	// ms timer
+	/*	timer++;
+	if(timer == 179){
+	clock++;
+	timer = 0;
+	}*/
+
+	// send function
+	if (isSending_IR()) {
+		processSend_IR(nTimer);
+	}
 }
-}
+
 ISR(INT0_vect) { // receive interrupt
-IR_processRecieve(nTimer, &IRdata);
-}/*/
+	processRecieve_IR(nTimer, &IRdata);
+}
--- Bomberman/Libraries/Hit/checkHit.h
+++ Bomberman/Libraries/Hit/checkHit.h
@@ -4,7 +4,6 @@
 #include "../Includes/Includes.h"
 
 void checkPlayerHit(uint8_t player_x, uint8_t player_y, uint8_t *hit, uint8_t grid[16][12]);
-void updateLives(uint8_t* hit, uint8_t* lives, MI0283QT9 lcd, uint8_t score);
 
 #endif
 
