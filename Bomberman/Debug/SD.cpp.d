6bbf818ebb9eb459b5b7c2a2c377890db5dece47
Martijn <Martijn@MARTIJN>
2016-12-05 10:53:41 +0100

    Commit to pull

---
+++ Bomberman/Debug/SD.cpp.d
@@ -0,0 +1,20 @@
+C:\Users\Martijn\AppData\Local\Temp\VMicroBuilds\Bomberman\uno\SD.cpp.o: \
+ C:\Arduino\libraries\SD\src\SD.cpp C:\Arduino\libraries\SD\src\SD.h \
+ C:\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
+ C:\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ C:\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
+ C:\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ C:\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ C:\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
+ C:\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ C:\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Arduino\hardware\arduino\avr\variants\standard/pins_arduino.h \
+ C:\Arduino\libraries\SD\src/utility/SdFat.h \
+ C:\Arduino\libraries\SD\src/utility/Sd2Card.h \
+ C:\Arduino\libraries\SD\src/utility/Sd2PinMap.h \
+ C:\Arduino\libraries\SD\src/utility/SdInfo.h \
+ C:\Arduino\libraries\SD\src/utility/FatStructs.h \
+ C:\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ C:\Arduino\libraries\SD\src/utility/SdFatUtil.h
---
+++ Bomberman/Debug/SdFat.cpp.d
@@ -0,0 +1,38 @@
+C:\Users\Martijn\AppData\Local\Temp\VMicroBuilds\Bomberman\uno\SdFat.cpp.o: \
+ C:\Arduino\libraries\SDfat\SdFat.cpp C:\Arduino\libraries\SDfat\SdFat.h \
+ C:\Arduino\libraries\SDfat\src/SdSpiCard/SdSpiCard.h \
+ C:\Arduino\libraries\SDfat\src/SdSpiCard/../SystemInclude.h \
+ C:\Arduino\libraries\SDfat\src/SdSpiCard/../FatLib/SysCall.h \
+ C:\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
+ C:\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ C:\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
+ C:\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ C:\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ C:\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
+ C:\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ C:\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Arduino\hardware\arduino\avr\variants\standard/pins_arduino.h \
+ C:\Arduino\hardware\arduino\avr\libraries\SPI\src/SPI.h \
+ C:\Arduino\libraries\SDfat\src/SdSpiCard/../SdFatConfig.h \
+ C:\Arduino\libraries\SDfat\src/SdSpiCard/SdInfo.h \
+ C:\Arduino\libraries\SDfat\src/SdSpiCard/SdSpi.h \
+ C:\Arduino\libraries\SDfat\src/FatLib/FatLib.h \
+ C:\Arduino\libraries\SDfat\src/FatLib/ArduinoFiles.h \
+ C:\Arduino\libraries\SDfat\src/FatLib/FatLibConfig.h \
+ C:\Arduino\libraries\SDfat\src/FatLib/../SdFatConfig.h \
+ C:\Arduino\libraries\SDfat\src/FatLib/FatFile.h \
+ C:\Arduino\libraries\SDfat\src/FatLib/FatApiConstants.h \
+ C:\Arduino\libraries\SDfat\src/FatLib/FatStructs.h \
+ C:\Arduino\libraries\SDfat\src/FatLib/FatVolume.h \
+ C:\Arduino\libraries\SDfat\src/FatLib/SysCall.h \
+ C:\Arduino\libraries\SDfat\src/FatLib/ArduinoStream.h \
+ C:\Arduino\libraries\SDfat\src/FatLib/bufstream.h \
+ C:\Arduino\libraries\SDfat\src/FatLib/iostream.h \
+ C:\Arduino\libraries\SDfat\src/FatLib/istream.h \
+ C:\Arduino\libraries\SDfat\src/FatLib/ios.h \
+ C:\Arduino\libraries\SDfat\src/FatLib/ostream.h \
+ C:\Arduino\libraries\SDfat\src/FatLib/FatFileSystem.h \
+ C:\Arduino\libraries\SDfat\src/FatLib/StdioStream.h \
+ C:\Arduino\libraries\SDfat\src/FatLib/fstream.h
