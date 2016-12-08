2f179219378ffd40a9bbbd0933b14084133c72c4
MikeSchotman <mikeschotman@gmail.com>
2016-12-08 10:16:18 +0100

    testcommit

--- Bomberman/Libraries/Hit/checkHit.cpp
+++ Bomberman/Libraries/Hit/checkHit.cpp
@@ -1,6 +1,4 @@
 #include "checkHit.h"
-uint8_t counter2 = 0;
-
 void checkPlayerHit(uint8_t player1_x, uint8_t player1_y, uint8_t *hit, uint8_t grid[16][12]) {
 	if ((grid[player1_x][player1_y] == 7 || grid[player1_x][player1_y] == 8 || grid[player1_x][player1_y] == 9)) {
 		(*hit) = 1;
