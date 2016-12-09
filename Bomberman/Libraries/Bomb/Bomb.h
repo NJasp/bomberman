bc6ab05680f3e03a72786cb71cc049a9d53927ee
Nils Jaspers <cat@national.shitposting.agency>
2016-12-09 14:11:16 +0100

    IR added to bomb placement

--- Bomberman/Libraries/Bomb/Bomb.h
+++ Bomberman/Libraries/Bomb/Bomb.h
@@ -5,7 +5,7 @@
 
 void draw_Explosion(MI0283QT9 screen, uint8_t bombradius, uint8_t grid[16][12], uint8_t* livebombs, uint8_t* score, uint8_t* hit, uint8_t player1_x, uint8_t player1_y);
 void clear_Explosion(MI0283QT9 screen, uint8_t bombradius, uint8_t grid[16][12]);
-void check_Bomb(uint8_t player1_x, uint8_t player1_y, uint8_t* player1_x_bombdrop, uint8_t* player1_y_bombdrop, uint8_t max_bombs, uint8_t* livebombs, uint8_t* antiholdCounter, uint8_t nunchuck_buf[], uint8_t grid[16][12]);
+void check_Bomb(uint8_t player1_x, uint8_t player1_y, uint8_t* player1_x_bombdrop, uint8_t* player1_y_bombdrop, uint8_t max_bombs, uint8_t* livebombs, uint8_t* antiholdCounter, uint8_t nunchuck_buf[], uint8_t grid[16][12], volatile uint8_t* isSendingIR);
 void draw_Bomb(uint8_t player1_x, uint8_t player1_y, uint8_t* player1_x_bombdrop, uint8_t* player1_y_bombdrop, MI0283QT9 screen);
 #endif
 
