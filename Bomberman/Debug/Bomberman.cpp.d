a1835df767f12d2dacf1456c45301417660a56ab
gebruiker-pc <koensleurink@hotmail.com>
2016-12-12 12:22:33 +0100

    Pushed dummy proof version for Martijn

--- Bomberman/Debug/Bomberman.cpp.d
+++ Bomberman/Debug/Bomberman.cpp.d
@@ -1,5 +1,5 @@
-C:\Users\mikes_000\AppData\Local\Temp\VMicroBuilds\Bomberman\uno\Bomberman.cpp.o: \
- C:\Users\mikes_000\AppData\Local\Temp\VMicroBuilds\Bomberman\uno\Bomberman.cpp \
+C:\Users\gebruiker-pc\AppData\Local\Temp\VMicroBuilds\Bomberman\uno\Bomberman.cpp.o: \
+ C:\Users\gebruiker-pc\AppData\Local\Temp\VMicroBuilds\Bomberman\uno\Bomberman.cpp \
  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/arduino.h \
  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
@@ -11,38 +11,32 @@ C:\Users\mikes_000\AppData\Local\Temp\VMicroBuilds\Bomberman\uno\Bomberman.cpp.o
  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\standard/pins_arduino.h \
- C:\Users\mikes_000\AppData\Local\Temp\VMicroBuilds\Bomberman\uno\Libraries/Levels/Levels.h \
- C:\Users\mikes_000\AppData\Local\Temp\VMicroBuilds\Bomberman\uno\Libraries/Levels/../Includes/Includes.h \
+ C:\Users\gebruiker-pc\AppData\Local\Temp\VMicroBuilds\Bomberman\uno\Libraries/Levels/Levels.h \
+ C:\Users\gebruiker-pc\AppData\Local\Temp\VMicroBuilds\Bomberman\uno\Libraries/Levels/../Includes/Includes.h \
  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
  C:\Program\ Files\ (x86)\Arduino\libraries\MI0283QT9/MI0283QT9.h \
  C:\Program\ Files\ (x86)\Arduino\libraries\GraphicsLib/GraphicsLib.h \
  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
- C:\Users\mikes_000\AppData\Local\Temp\VMicroBuilds\Bomberman\uno\Libraries/Levels/../Includes/../IR/IR.h \
+ C:\Users\gebruiker-pc\AppData\Local\Temp\VMicroBuilds\Bomberman\uno\Libraries/Levels/../Includes/../IR/IR.h \
  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\Wire\src/Wire.h \
  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\SPI\src/SPI.h \
  C:\Program\ Files\ (x86)\Arduino\libraries\GraphicsLib/BMPheader.h \
- C:\Program\ Files\ (x86)\Arduino\libraries\SD\src/SD.h \
- C:\Program\ Files\ (x86)\Arduino\libraries\SD\src/utility/SdFat.h \
- C:\Program\ Files\ (x86)\Arduino\libraries\SD\src/utility/Sd2Card.h \
- C:\Program\ Files\ (x86)\Arduino\libraries\SD\src/utility/Sd2PinMap.h \
- C:\Program\ Files\ (x86)\Arduino\libraries\SD\src/utility/SdInfo.h \
- C:\Program\ Files\ (x86)\Arduino\libraries\SD\src/utility/FatStructs.h \
- C:\Program\ Files\ (x86)\Arduino\libraries\SD\src/utility/SdFatUtil.h \
- C:\Users\mikes_000\AppData\Local\Temp\VMicroBuilds\Bomberman\uno\Libraries/LCD/LCD.h \
- C:\Users\mikes_000\AppData\Local\Temp\VMicroBuilds\Bomberman\uno\Libraries/LCD/../Includes/Includes.h \
- C:\Users\mikes_000\AppData\Local\Temp\VMicroBuilds\Bomberman\uno\Libraries/Nunchuck/Nunchuck.h \
- C:\Users\mikes_000\AppData\Local\Temp\VMicroBuilds\Bomberman\uno\Libraries/Nunchuck/../Includes/Includes.h \
- C:\Users\mikes_000\AppData\Local\Temp\VMicroBuilds\Bomberman\uno\Libraries/DebugTools/DebugTools.h \
- C:\Users\mikes_000\AppData\Local\Temp\VMicroBuilds\Bomberman\uno\Libraries/DebugTools/../Includes/Includes.h \
- C:\Users\mikes_000\AppData\Local\Temp\VMicroBuilds\Bomberman\uno\Libraries/Player/Player.h \
- C:\Users\mikes_000\AppData\Local\Temp\VMicroBuilds\Bomberman\uno\Libraries/Player/../Includes/Includes.h \
- C:\Users\mikes_000\AppData\Local\Temp\VMicroBuilds\Bomberman\uno\Libraries/Bomb/Bomb.h \
- C:\Users\mikes_000\AppData\Local\Temp\VMicroBuilds\Bomberman\uno\Libraries/Bomb/../Includes/Includes.h \
- C:\Users\mikes_000\AppData\Local\Temp\VMicroBuilds\Bomberman\uno\Libraries/MSD_shield/mSD_shield.h \
- C:\Users\mikes_000\AppData\Local\Temp\VMicroBuilds\Bomberman\uno\Libraries/MSD_shield/../Includes/Includes.h \
- C:\Users\mikes_000\AppData\Local\Temp\VMicroBuilds\Bomberman\uno\Libraries/IR/IR.h \
- C:\Users\mikes_000\AppData\Local\Temp\VMicroBuilds\Bomberman\uno\Libraries/Hit/checkHit.h \
- C:\Users\mikes_000\AppData\Local\Temp\VMicroBuilds\Bomberman\uno\Libraries/Hit/../Includes/Includes.h \
- C:\Users\mikes_000\AppData\Local\Temp\VMicroBuilds\Bomberman\uno\Libraries/Menu/Menu.h \
- C:\Users\mikes_000\AppData\Local\Temp\VMicroBuilds\Bomberman\uno\Libraries/Menu/../Includes/Includes.h
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\EEPROM\src/eeprom.h \
+ C:\Users\gebruiker-pc\AppData\Local\Temp\VMicroBuilds\Bomberman\uno\Libraries/LCD/LCD.h \
+ C:\Users\gebruiker-pc\AppData\Local\Temp\VMicroBuilds\Bomberman\uno\Libraries/LCD/../Includes/Includes.h \
+ C:\Users\gebruiker-pc\AppData\Local\Temp\VMicroBuilds\Bomberman\uno\Libraries/Nunchuck/Nunchuck.h \
+ C:\Users\gebruiker-pc\AppData\Local\Temp\VMicroBuilds\Bomberman\uno\Libraries/Nunchuck/../Includes/Includes.h \
+ C:\Users\gebruiker-pc\AppData\Local\Temp\VMicroBuilds\Bomberman\uno\Libraries/DebugTools/DebugTools.h \
+ C:\Users\gebruiker-pc\AppData\Local\Temp\VMicroBuilds\Bomberman\uno\Libraries/DebugTools/../Includes/Includes.h \
+ C:\Users\gebruiker-pc\AppData\Local\Temp\VMicroBuilds\Bomberman\uno\Libraries/Player/Player.h \
+ C:\Users\gebruiker-pc\AppData\Local\Temp\VMicroBuilds\Bomberman\uno\Libraries/Player/../Includes/Includes.h \
+ C:\Users\gebruiker-pc\AppData\Local\Temp\VMicroBuilds\Bomberman\uno\Libraries/Bomb/Bomb.h \
+ C:\Users\gebruiker-pc\AppData\Local\Temp\VMicroBuilds\Bomberman\uno\Libraries/Bomb/../Includes/Includes.h \
+ C:\Users\gebruiker-pc\AppData\Local\Temp\VMicroBuilds\Bomberman\uno\Libraries/IR/IR.h \
+ C:\Users\gebruiker-pc\AppData\Local\Temp\VMicroBuilds\Bomberman\uno\Libraries/Menu/Menu.h \
+ C:\Users\gebruiker-pc\AppData\Local\Temp\VMicroBuilds\Bomberman\uno\Libraries/Menu/../Includes/Includes.h \
+ C:\Users\gebruiker-pc\AppData\Local\Temp\VMicroBuilds\Bomberman\uno\Libraries/Hit/checkHit.h \
+ C:\Users\gebruiker-pc\AppData\Local\Temp\VMicroBuilds\Bomberman\uno\Libraries/Hit/../Includes/Includes.h \
+ C:\Users\gebruiker-pc\AppData\Local\Temp\VMicroBuilds\Bomberman\uno\Libraries/Sprites/Sprites.h \
+ C:\Users\gebruiker-pc\AppData\Local\Temp\VMicroBuilds\Bomberman\uno\Libraries/Sprites/../Includes/Includes.h
