0bbba9ffdb513237ebf27d300fb290e97448dd12
Nils Jaspers <cat@national.shitposting.agency>
2016-12-19 12:31:12 +0100

    check before drawing bomb

--- Bomberman/Bomberman.ino
+++ Bomberman/Bomberman.ino
@@ -136,11 +136,10 @@ int main() {
 					lcd.fillRect(player2_x_old * 20, player2_y_old * 20, 20, 20, Background);
 
 					// draw the bomb again when drawing over it
-					if (player2_x_old == player2_x_bombdrop && player2_y_old == player2_y_bombdrop) {
+					if (player2_x_old == player2_x_bombdrop && player2_y_old == player2_y_bombdrop && !grid[player2_x_bombdrop][player2_y_bombdrop]) {
 						draw_BombSprite(lcd, player2_x_bombdrop, player1_y_bombdrop);
 					}
 
-					lcd.fillRect(player2_x_old * 20, player2_y_old * 20, 20, 20, Background);
 					lcd.fillRect(player2_x * 20, player2_y * 20, 20, 20, RGB(0, 0, 255));
 				}
 
@@ -161,7 +160,7 @@ int main() {
 					else {
 						send_IR(&isSendingIR, PLAYER, player1_x, player1_y);
 					}
-					if(bombDelayCounter < 3) {
+					if(bombDelayCounter > 3) {
 						bombDelayCounter = 0;
 						for (rowCounter = 0; rowCounter < 12; rowCounter++) {
 							for (collumnCounter = 0; collumnCounter < 16; collumnCounter++) {
